bdeef8c90913447798e3711eb54e2b1484bd6fd0
Michael Rodriguez <mikeyrod22@gmail.com>
2021-03-26 19:38:26 -0500

    search bar does stuff with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
-let citySearchButtonEl = document.getElementById("city-search-button");
 let citySearchTextEl = document.getElementById("city-search-text");
+let citySearchButtonEl = document.getElementById("city-search-button");
+let searchULEl = document.getElementById("search-bar-ul");
+let clearButtonEl = document.getElementById("clear-button");
 let userSearch;
 
+
 function citySearch() {
-    userSearch= citySearchTextEl.value;
-    console.log(userSearch);
+    userSearch = citySearchTextEl.value;
+    if (userSearch) {
+        document.getElementById("forecast-city-el").innerText = userSearch;
+        let newLI = document.createElement("li");
+        newLI.innerText = userSearch;
+        searchULEl.prepend(newLI);
+        citySearchTextEl.value = "";
+    }
 }
 
 citySearchButtonEl.addEventListener("click", citySearch);
+citySearchTextEl.addEventListener("keypress", function(e){
+    if (e.key === "Enter" ) {
+        citySearch()
+    }
+})
+
+function clearHistory() {
+    while (searchULEl.firstChild) {
+        searchULEl.removeChild(searchULEl.firstChild);
+    }
+}
+
+clearButtonEl.addEventListener("click", clearHistory);
