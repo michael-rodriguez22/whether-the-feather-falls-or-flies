62c19c6d86c079c4a49d994781594e2caa460892
Michael Rodriguez <mikeyrod22@gmail.com>
2021-03-28 00:49:35 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,15 @@
-let citySearchTextEl = document.getElementById("city-search-text");
-let citySearchButtonEl = document.getElementById("city-search-button");
-let searchULEl = document.getElementById("search-bar-ul");
-let clearButtonEl = document.getElementById("clear-button");
-let forecastCityEl = document.getElementById("forecast-city-el");
-let currentDateEl = document.getElementById("current-date-el");
-let userSearch;
-let cityName;
-let cityLat;
-let cityLon;
-let callData;
+// DATES
 
+let currentDateEl = document.getElementById("current-date-el");
 function displayDates() {
     currentDateEl.innerText = Date();
 }
 displayDates();
 const updateTime = setInterval(displayDates, 1000);
 
+// SEARCH FOR CITY
 function citySearch() {
-    userSearch = citySearchTextEl.value;
+    let userSearch = citySearchTextEl.value;
     if (userSearch) {
         // get lat and lon from city name
         citySearchTextEl.value = "";
@@ -26,8 +18,8 @@ function citySearch() {
                 cityResponse.json()
                     .then(function (cityData) {
                         cityName = cityData.name;
-                        cityLat = Number(cityData.coord.lat);
-                        cityLon = Number(cityData.coord.lon);
+                        let cityLat = Number(cityData.coord.lat);
+                        let cityLon = Number(cityData.coord.lon);
                         // get required info from lat and lon                 
                         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&appid=${key}`)
                             .then(function (llResponse) {
@@ -45,12 +37,12 @@ function citySearch() {
     }
 }
 
+// SEARCH HISTORY
 let keyIterator = 1;
 let savedSearchObject = new Object();
 function saveSearch() {
     savedSearchObject.name = cityName;
     savedSearchObject.data = JSON.stringify(callData);
-    // console.log(savedSearchObject);
     localStorage.setItem(JSON.stringify(keyIterator), JSON.stringify(savedSearchObject))
     keyIterator++;
 }
@@ -63,9 +55,25 @@ function clearHistory() {
     keyIterator = 1;
 }
 
+// DISPLAY INFORMATION ON PAGE
+let cityName;
+let callData;
+let searchULEl = document.getElementById("search-bar-ul");
+let forecastCityEl = document.getElementById("forecast-city-el");
+let currentTempEl = document.getElementById("current-temp-el");
+let currentHumidityEl = document.getElementById("current-humidity-el");
+let currentWindSpeedEl = document.getElementById("current-wind-speed-el");
+let currentUVIndexEl = document.getElementById("current-uv-index-el");
+
 function displayData() {
     forecastCityEl.innerText = cityName;
+
 }
+
+// EVENT LISTENERS
+let citySearchTextEl = document.getElementById("city-search-text");
+let citySearchButtonEl = document.getElementById("city-search-button");
+let clearButtonEl = document.getElementById("clear-button");
 
 citySearchTextEl.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
